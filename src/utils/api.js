a5574c2b1bb56397bce6dc93a88abc4ffea50f5b
solo092 <thamerown@gmail.com>
2026-08-24 02:52:39 -0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'https://lamsa-backend-7c1lzhoe3-thamer7.vercel.app/api';
+
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
